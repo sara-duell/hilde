fd51f4d98d9c7140b814ce846e57556abaa8a758
sara duell <116595087+sara-duell@users.noreply.github.com>
2022-12-09 12:28:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,24 +2,21 @@
 $( document ).ready(function() {
     console.log( "the html has loaded and my script file is properly loaded into my website code" );
     
-// get hours for title, and minutes for rotation
-var s = new Date().getSeconds();
-var m = new Date().getMinutes();
-var h = new Date().getHours();
+    var morning = "morning.gif";
+    var afternoon = "afternoon.gif";
+    var night = "night.gif";
 
-// audio; begins playing at the corresponding minute and second mark of the time when the website is loaded
-function theHoursOST(){
-  var theHoursOST = new Audio;
-  theHoursOST.src = `audio/hilde.mp3`;
-  theHoursOST.play();
-  theHoursOST.loop = true;
-  theHoursOST.autoplay = true;
 
-  // audio on mobile
-  body.addEventListener('click', function(e){
-    theHoursOST.play();
-    theHoursOST.loop = true;
-    theHoursOST.autoplay = true;
-  })
-  // console.log(`audio/hilde.mp3#t=00:${m}:${s}`);
-}
+    setInterval(function() {
+   var d = new Date(),
+       hours = d.getHours(),
+       min = d.getMinutes(),
+       sec = d.getSeconds();
+   if (hours === 12 && min === 29 && sec === 01) {
+       var audio = document.getElementsByTagName('audio')[0]
+       //add the time in seconds
+       audio.currentTime = 0;
+       audio.play()
+       audio.style.display = 'block'
+   }
+}, 1000)
